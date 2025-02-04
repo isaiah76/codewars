67f8aecd64380fb6127a80efbac6c9d76d9174f7
Isaiah512 <fdscssdccefb@gmail.com>
2025-02-04 21:14:09 +0800

    Beginner - Lost Without a Map

---
+++ 8kyu/BeginnerLostWithoutAMap/Maps.java
@@ -0,0 +1,7 @@
+public class Maps {
+  public static int[] map(int[] arr){
+    int[] result = new int[arr.length];
+    for (int i = 0; i < arr.length; i++) result[i] = arr[i] * 2;
+    return result;
+  }
+}
