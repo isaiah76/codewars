e4bf931ec2ce678a825ee044a848ec5cf5fbfe2d
Isaiah512 <fdscssdccefb@gmail.com>
2025-02-14 22:03:34 +0800

    Thinkful - Logic Drills:Traffic light

---
+++ 8kyu/ThinkfulLogicDrillsTrafficLight/TrafficLights.java
@@ -0,0 +1,5 @@
+public class TrafficLights {
+  public static String updateLight(String current) {
+    return current.equals("green") ? "yellow" : current.equals("yellow") ? "red" : "green";
+  }
+}
