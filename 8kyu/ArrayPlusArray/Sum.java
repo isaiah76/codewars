34d10b4cdd8c3f27e4c21228ec2a7b20f28e244f
Isaiah512 <fdscssdccefb@gmail.com>
2025-02-17 23:25:56 +0800

    Array Plus Array

---
+++ 8kyu/ArrayPlusArray/Sum.java
@@ -0,0 +1,8 @@
+public class Sum {
+  public static int arrayPlusArray(int[] arr1, int[] arr2) {
+    int sum = 0;
+    for (int i:arr1) sum += i;
+    for (int i:arr2) sum += i;
+    return sum;
+  }
+}
