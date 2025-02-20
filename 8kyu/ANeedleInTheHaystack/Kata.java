7d2904ca0527c99836e2fc26b441f7d6e9acff70
Isaiah512 <fdscssdccefb@gmail.com>
2025-02-21 01:52:49 +0800

    A Needle in the Haystack

---
+++ 8kyu/ANeedleInTheHaystack/Kata.java
@@ -0,0 +1,6 @@
+public class Kata {
+  public static String findNeedle(Object[] haystack) {
+    for (int i = 0; i < haystack.length; i++) if ("needle".equals(haystack[i])) return "found the needle at position "+i;
+    return "";
+  }
+}
