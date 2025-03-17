d3127defb00ff799d2397814617a260a581a1070
Isaiah512 <fdscssdccefb@gmail.com>
2025-03-17 20:24:10 +0800

    Student's Final Grade

---
+++ java/8kyu/StudentsFinalGrade/StudentFinalGrade.java
@@ -0,0 +1,6 @@
+public class StudentFinalGrade{    
+    public static int finalGrade(int exam, int projects){
+        return exam > 90 || projects > 10 ? 100 : (exam > 75 && projects >= 5) ? 90 :
+        (exam > 50 && projects >= 2) ? 75 : 0;
+    }
+}
